259614c518e74b56eea4240a92106c98c3bdab6a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-04 03:39:33 +0100

    refactor createUser() function and format document with prettier

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const http = require("http");
-const { usersDbPath, booksDbPath, readDatabase, writeToDb, getRequestData, parseDatabaseStringValue, authenticateUser } = require('./utils');
+const {
+  usersDbPath,
+  booksDbPath,
+  readDatabase,
+  writeToDb,
+  getRequestData,
+  parseDatabaseStringValue,
+  authenticateUser,
+} = require("./utils");
 
 const server = http.createServer(serverListener);
 
@@ -11,30 +19,30 @@ async function serverListener(req, res) {
     if (req.url === "/user/create" && req.method === "POST") {
       await createUser(req, res, requestData);
     } else if (req.url === "/users") {
-      await authenticateUser(req, res, ["admin"], requestData.userLogin)
+      await authenticateUser(req, res, ["admin"], requestData.userLogin);
       getAllUsers(req, res);
     } else if (req.url === "/book" && req.method === "POST") {
-      await authenticateUser(req, res, ["admin"], requestData.userLogin)
+      await authenticateUser(req, res, ["admin"], requestData.userLogin);
       await createBook(req, res, requestData.data);
     } else if (req.url === "/book" && req.method === "PATCH") {
-      await authenticateUser(req, res, ["admin"], requestData.userLogin)
+      await authenticateUser(req, res, ["admin"], requestData.userLogin);
       updateBook(req, res);
     } else if (req.url === "/book" && req.method === "DELETE") {
-      await authenticateUser(req, res, ["admin"], requestData.userLogin)
+      await authenticateUser(req, res, ["admin"], requestData.userLogin);
       deleteBook(req, res);
     } else if (req.url === "/book/loan" && req.method === "POST") {
       loanOutBook(req, res);
     } else if (req.url === "/book/return" && req.method === "POST") {
       returnLoanedBook(req, res);
     } else {
-        res.statusCode = 404;
-        res.end("The route does not exists.")
+      res.statusCode = 404;
+      res.end("The route does not exists.");
     }
-  } catch(err) {
+  } catch (err) {
     console.log(err);
     res.statusCode = 500;
     res.end(err);
-  };
+  }
 }
 
 async function getAllUsers(req, res) {
@@ -42,15 +50,13 @@ async function getAllUsers(req, res) {
     const users = await readDatabase(usersDbPath);
     res.end(users);
   } catch (error) {
-    res.end(error)
+    res.end(error);
   }
 }
 
 async function createUser(req, res, userData) {
   try {
-    const allRegisteredUsers = await readDatabase(usersDbPath);
-
-    const users = parseDatabaseStringValue(allRegisteredUsers)
+    const users = parseDatabaseStringValue(await readDatabase(usersDbPath));
 
     const userExists = users.find((user) => {
       return user.username === userData.username;
@@ -62,11 +68,12 @@ async function createUser(req, res, userData) {
 
     users.push(userData);
     await writeToDb(users, usersDbPath);
-    res.end(JSON.stringify({
-      message: "User created successfully",
-      user: userData
-    }))
-
+    res.end(
+      JSON.stringify({
+        message: "User created successfully",
+        user: userData,
+      })
+    );
   } catch (error) {
     console.log(error);
     res.statusCode = 400;
@@ -77,22 +84,23 @@ async function createUser(req, res, userData) {
 async function createBook(req, res, newBook) {
   try {
     const books = parseDatabaseStringValue(await readDatabase(booksDbPath));
-    const bookExists = books.find(book => {
+    const bookExists = books.find((book) => {
       return book.isbn === newBook.isbn;
-    })
+    });
     if (bookExists) {
       res.statusCode = 405;
-      return res.end("Book already exists!")
+      return res.end("Book already exists!");
     }
 
     books.push(newBook);
-    const newBooks = await writeToDb(books, booksDbPath);
+    await writeToDb(books, booksDbPath);
 
-    res.end(JSON.stringify({
-      "message": "Book created succesfully!",
-      "book": newBook
-    }));
-
+    res.end(
+      JSON.stringify({
+        message: "Book created succesfully!",
+        book: newBook,
+      })
+    );
   } catch (error) {
     res.statusCode = 500;
     res.end(error);
